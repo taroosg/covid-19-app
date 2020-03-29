a27db927cb4eead291df8f2aa8435a5b261db75d
taroosg <macchinetta.xyz@gmail.com>
2020-03-29 20:13:27 +0900

    fix ogp

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Helmet } from "react-helmet";
 import './index.css';
-import banner from './banner.jpg';
+// import banner from './banner.jpg';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
@@ -13,7 +13,7 @@ ReactDOM.render(
       meta={[
         { name: 'twitter:card', content: 'COVID-19-app' },
         { name: "twitter:card", content: "summary_large_image" },
-        { property: 'og:image', content: banner },
+        { property: 'og:image', content: 'https://taroosg-covid-19-app.netlify.com/banner.jpg' },
         { property: 'og:title', content: 'COVID-19' },
         { property: 'og:description', content: 'COVID-19-app' },
         { property: 'og:url', content: process.env.PUBLIC_URL },
