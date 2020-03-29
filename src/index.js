7f2dded3873b63ab97f91b9e9c5441caa940a4da
taroosg <macchinetta.xyz@gmail.com>
2020-03-29 21:18:20 +0900

    fix ogp

--- src/index.js
+++ src/index.js
@@ -11,14 +11,6 @@ ReactDOM.render(
     <Helmet
       title={'COVID-19'}
       meta={[
-        // { name: "twitter:card", content: "summary_large_image" },
-        // { name: 'twitter:image', content: 'https://taroosg-covid-19-app.netlify.com/banner.jpg' },
-        // { property: 'og:image', content: 'https://taroosg-covid-19-app.netlify.com/banner.jpg' },
-        // { property: 'og:title', content: 'COVID-19' },
-        // { property: 'og:site_name', content: 'COVID-19' },
-        // { property: 'og:type', content: 'website' },
-        // { property: 'og:description', content: 'COVID-19-app' },
-        // { property: 'og:url', content: 'https://taroosg-covid-19-app.netlify.com' },
         { name: 'twitter:card', content: 'summary_large_image' },
         { name: 'twitter:site', content: `@taro_osg` },
         { name: 'twitter:creator', content: `@taro_osg` },
