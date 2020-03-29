951d153e7ee4b7a7f21ee0e30fbb3d4be093e249
taroosg <macchinetta.xyz@gmail.com>
2020-03-29 14:29:22 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <main className="App-main">
+        <p>
+          <code>COVID-19</code>
+        </p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
